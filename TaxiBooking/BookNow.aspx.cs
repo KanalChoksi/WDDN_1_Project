7c3f9dd11d843a44f6b8dd556112f270d8d68fb3
Shruti Makwana <shrutimak810@gmail.com>
2021-10-19 09:39:23 +0530

    Final Commit

---
+++ TaxiBooking/BookNow.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TaxiBooking
+{
+    public partial class BookNow : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["login"] != null)
+            {
+                TextBox5.Text = ((int)Session["kilometers"] * 10).ToString();
+
+            }
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            label.Text = "<h1>Payment Done successfully !!</h1>";
+        }
+
+    }
+}
